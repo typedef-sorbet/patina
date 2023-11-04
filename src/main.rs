e5b7aa1f9ec395d6ded04c40c207af961af27837
Spencer Warneke <warnespe001@gmail.com>
2023-11-03 23:14:38 -0700

    One move is confirmed to work.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use chess::{Board, MoveGen, BoardStatus, ChessMove, Piece, BitBoard, Square, EMPTY};
+use chess::{Board, ChessMove, Piece, BitBoard, Square, EMPTY, Color};
 use regex::Regex;
 
 struct Game {
@@ -11,35 +11,40 @@ struct Game {
 struct MoveRequest {
     piece_type: Piece,
     square_from: Option<Square>,
-    square_to: Square
+    square_to: Square,
+    promotion: Option<Piece>
 }
 
 impl TryFrom<String> for MoveRequest {
     type Error = &'static str;
     fn try_from(s: String) -> Result<Self, Self::Error> {
         // I am a fool for trying to do this without regular expressions.
-        let re = Regex::new(r"(?:<square_from>[a-h][1-8])?(?:<piece_type>[NKQBR])?x?(?:<square_to>[a-h][1-8])[+#]?").unwrap();
+        let re = Regex::new(r"([a-h]?[1-8]?)([NKQBR]?)x?([a-h][1-8])[+#]?").unwrap();
 
         if let Some(captures) = re.captures(&s) {
-            let square_from_opt = captures.name("square_from");
-            let square_to_opt = captures.name("square_to");
-            let piece_type_opt = captures.name("piece_type");
+            let (_full, [square_from_str, piece_type_str, square_to_str]) = captures.extract();
 
-            let square_from = if square_from_opt.is_some() {Some(square_from_opt.unwrap().as_str().parse::<Square>().unwrap())} else {None};
-            let square_to = square_to_opt.unwrap().as_str().parse::<Square>().unwrap();
-            let piece_type = match piece_type_opt.unwrap().as_str().bytes().next().unwrap() as char {
-                'K' =>  Piece::King,
-                'Q' =>  Piece::Queen,
-                'N' =>  Piece::Knight,
-                'B' =>  Piece::Bishop,
-                'R' =>  Piece::Rook,
-                _ =>    Piece::Pawn
+            let square_from = if !square_from_str.is_empty() {Some(square_from_str.parse::<Square>().unwrap())} else {None};
+            let square_to = square_to_str.parse::<Square>().unwrap();
+            let piece_type = if !piece_type_str.is_empty() {
+                match piece_type_str.bytes().next().unwrap() as char {
+                    'K' =>  Piece::King,
+                    'Q' =>  Piece::Queen,
+                    'N' =>  Piece::Knight,
+                    'B' =>  Piece::Bishop,
+                    'R' =>  Piece::Rook,
+                    _ =>    Piece::Pawn
+                }
+            } else {
+                Piece::Pawn
             };
 
+            // TODO handle promotion syntax
             Ok(Self {
                 square_from,
                 square_to,
-                piece_type
+                piece_type,
+                promotion: None
             })
         } else {
             Err("Regex match failed")
@@ -54,6 +59,13 @@ fn main() {
         user_id_black: String::new(),
         whose_turn: chess::Color::White
     };
+
+    println!("{}", game.board);
+    println!("Making move e4");
+    match move_piece(&mut game, "e4".to_string()) {
+        Ok(())      => println!("{}", game.board),
+        Err(reason) => println!("Got error: {}", reason)
+    };
 }
 
 fn move_piece(game: &mut Game, movestr: String) -> Result<(), &str> {
@@ -61,32 +73,57 @@ fn move_piece(game: &mut Game, movestr: String) -> Result<(), &str> {
     if let Ok(move_requested) = MoveRequest::try_from(movestr) {
         let mut relevant_pieces: BitBoard = game.board.pieces(move_requested.piece_type).clone();
         let mut source_squares: Vec<Square> = Vec::new();
-    
-        while relevant_pieces != EMPTY {
-            let square = relevant_pieces.to_square();
-            relevant_pieces ^= EMPTY | BitBoard::from_square(square);
-            // Filter any pieces whose turn it isn't.
-            if game.board.color_on(square).unwrap() == game.whose_turn {
-                source_squares.push(square);
+        let source_square: Square;
+
+        if move_requested.square_from.is_none()
+        {
+            while relevant_pieces != EMPTY {
+                let square = relevant_pieces.to_square();
+                // xor-equals to clear this bit from the bitmap
+                relevant_pieces ^= EMPTY | BitBoard::from_square(square);
+                // Filter any pieces whose turn it isn't.
+                // NOTE: This unwrap() should be safe, since we're only looking at squares where a piece exists
+                if game.board.color_on(square).unwrap() == game.whose_turn {
+                    source_squares.push(square);
+                }
+            }
+        
+            // Are there any pieces left?
+            if source_squares.len() == 0 {
+                return Err("You have no pieces of that type.");
+            }
+
+            // Of the remaining pieces, can any move to the specified square?
+            let valid_source_squares = source_squares.iter()
+                                                    .map(|&sq| (game.board.legal(ChessMove::new(sq, move_requested.square_to, None)), sq))
+                                                    .filter(|(valid, _)| *valid)
+                                                    .collect::<Vec<(bool, Square)>>();
+        
+            if valid_source_squares.len() > 1 {
+                return Err("More than one legal move is implied by that notation -- prepend the piece name with the square of the piece you want to move")
+            }
+            else if valid_source_squares.len() == 0 {
+                return Err("Given movestring is not a legal move");
+            }
+            else {
+                source_square = valid_source_squares.as_slice()[0].1;
             }
         }
-    
-        // Are there any pieces left?
-        if source_squares.len() < 0 {
-            return Err("You have no pieces of that type.");
+        else {
+            source_square = move_requested.square_from.unwrap();
         }
-    
-        // Of the remaining pieces, can any move to the specified square?
-        let valid_source_squares = source_squares.iter()
-                                                .map(|&sq| (game.board.legal(ChessMove::new(sq, target_square, None)), ChessMove::new(sq, target_square, None)))
-                                                .filter(|(valid, _)| *valid)
-                                                .collect::<Vec<(bool, ChessMove)>>();
-    
-        if valid_source_squares.len() > 1 {
-            return Err("More than one move is implied by that notation -- prepend the piece name with the square of the piece you want to move")
-        }
+
+        // We now have a known good source and target square. Let's make a move!
+        // TODO handle promotion
+        game.board = game.board.make_move_new(ChessMove::new(source_square, move_requested.square_to, None));
+        game.whose_turn = match game.whose_turn {
+            Color::White => Color::Black,
+            Color::Black => Color::White
+        };
+
+        Ok(())
     }
-
-
-    Ok(())
+    else {
+        Err("Improperly formatted move string")
+    }
 }
